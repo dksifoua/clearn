5ffc44e3d86f6404a39e3606571f1f62e6b4d7cb
dksifoua <dimitri.sifoua@gmail.com>
2026-04-08 21:36:39 -0400

    Convert fahrenheit to celsius.

---
+++ celsius.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define LOWER   0
+#define STEP    20
+#define UPPER   300
+
+int main() {
+  float fahrenheit = LOWER, celsius;
+  printf("Fahrenheit\tCelsius\n");
+  while (fahrenheit <= UPPER) {
+    celsius = (5.0f / 9) * (fahrenheit - 32);
+    printf("%3.0f\t\t%5.1f\n", fahrenheit, celsius);
+
+    fahrenheit += STEP;
+  }
+
+  return 0;
+}

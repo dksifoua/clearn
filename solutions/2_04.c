2367aa288a26c5315bf328f02967eef0b932ad1b
dksifoua <dimitri.sifoua@gmail.com>
2026-06-03 23:56:45 -0400

    Add solution for ex. 2.4

---
+++ solutions/2_04.c
@@ -0,0 +1,29 @@
+// Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes
+// each character in s1 that matches any character in the string s2.
+
+#include <stdio.h>
+
+void squeeze(char s1[], const char s2[]);
+
+int main(void) {
+    char s1[] = "Write an alternative version of squeeze(s1,s2) that deletes each character in s1 that matches any character in the string s2.";
+    char s2[] = "aeuio";
+    
+    printf("%s\n", s1);
+    squeeze(s1, s2);
+    printf("%s\n", s1);
+    return 0;
+}
+
+void squeeze(char s1[], const char s2[]) {
+    int i, j, k;
+    for (i = k = 0; s1[i] != '\0'; ++i) {
+        for (j = 0; s2[j] != '\0' && s1[i] != s2[j]; ++j)
+            ;
+
+        if (s2[j] == '\0') {
+            s1[k++] = s1[i];
+        }
+    }
+    s1[k] = '\0';
+}

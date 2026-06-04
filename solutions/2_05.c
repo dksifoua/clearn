9819f81bf6b01991021773802a0582e87f5638ba
dksifoua <dimitri.sifoua@gmail.com>
2026-06-04 00:08:08 -0400

    add solution for ex. 2.5

---
+++ solutions/2_05.c
@@ -0,0 +1,27 @@
+// Exercise 2-5. Write the function any(s1,s2), which returns the first location in a string s1
+// where any character from the string s2 occurs, or -1 if s1 contains no characters from s2.
+// (The standard library function strpbrk does the same job but returns a pointer to the
+// location.)
+
+#include <stdio.h>
+
+int any(const char s1[], const char s2[]);
+
+int main(void) {
+    const char s1[] = "location";
+    const char s2[] = "aeiou";
+    printf("%s - %s = %d\n", s1, s2, any(s1, s2));
+    return 0;
+}
+
+int any(const char s1[], const char s2[]) {
+    for (int i = 0; s1[i] != '\0'; ++i) {
+        for (int j = 0; s2[j] != '\0'; ++j) {
+            if (s1[i] == s2[j]) {
+                return i;
+            }
+        }
+    }
+    
+    return -1;
+}

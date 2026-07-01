34536b4ae3ccf8453bc9378fb713bf19790e670f
dksifoua <dimitri.sifoua@gmail.com>
2026-06-30 22:24:00 -0400

    ex: solve 2_07

---
+++ solutions/2_07.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned invert(unsigned x, int p, int n);
+
+int main(void) {
+    // 1011 0010 = x = 2 + 16 + 32 + 128 = 178
+    // 1000 1110 = r = 128 + 8 + 4 + 2 = 142
+    unsigned x = 0b10110010, r = 0b10001110;
+    int p = 5, n = 4;
+    printf("invert(%u, %d, %d) = %u (Expected = %u)\n", x, p, n, invert(x, p, n), r);
+    return 0;
+}
+
+unsigned invert(unsigned x, int p, int n) {
+    // 1011 0010 = x
+    // 0011 1100 = mask = 4 + 8 + 16 + 32 = 60
+    // 1100 1110 = r
+    unsigned mask = ~(~0u << n) << (p - n + 1);
+    return x ^ mask; // 
+}

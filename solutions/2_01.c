24f8a5a4c8a86c912429b10cd33b556acd27db7b
dksifoua <dimitri.sifoua@gmail.com>
2026-07-01 00:13:55 -0400

    fix: update solutions for 2_01 and 2_07

--- solutions/2_01.c
+++ solutions/2_01.c
@@ -7,34 +7,135 @@
 #include <limits.h>
 #include <stdio.h>
 
+int char_wordlength(void);
+int short_wordlength(void);
+int int_wordlength(void);
+int long_wordlength(void);
+int longlong_wordlength(void);
+int float_wordlength(void);
+int double_wordlength(void);
+int longdouble_wordlength(void);
+
 int main(void) {
-    printf("======================== Ranges from standard headers =================\n");
-    printf("%25s: [%20d, %20d]\n", "(signed) char", CHAR_MIN, CHAR_MAX);
-    printf("%25s: [%20d, %20d]\n", "unsigned char", 0, UCHAR_MAX);
-    printf("%25s: [%20d, %20d]\n", "(signed) short (int)", SHRT_MIN, SHRT_MAX);
-    printf("%25s: [%20d, %20u]\n", "unsigned short (int)", 0, USHRT_MAX);
-    printf("%25s: [%20d, %20d]\n", "(signed) int", INT_MIN, INT_MAX);
-    printf("%25s: [%20d, %20u]\n", "unsigned int", 0, UINT_MAX);
-    printf("%25s: [%20ld, %20ld]\n", "(signed) long (int)", LONG_MIN, LONG_MAX);
-    printf("%25s: [%20ld, %20lu]\n", "unsigned long (int)", 0L, ULONG_MAX);
-    printf("%25s: [%20lld, %20lld]\n", "(signed) long long (int)", LLONG_MIN, LLONG_MAX);
-    printf("%25s: [%20lld, %20llu]\n", "unsigned long long (int)", 0LL, ULLONG_MAX);
-    printf("%25s: [%20E, %20E]\n", "float", FLT_MIN, FLT_MAX);
-    printf("%25s: [%20lE, %20lE]\n", "double", DBL_MIN, DBL_MAX);
-    printf("%25s: [%20LE, %20LE]\n", "long double", LDBL_MIN, LDBL_MAX);
+  printf("======================== Ranges from standard headers\n");
+  printf("%25s: [%20d, %20d]\n", "(signed) char", CHAR_MIN, CHAR_MAX);
+  printf("%25s: [%20d, %20d]\n", "unsigned char", 0, UCHAR_MAX);
+  printf("%25s: [%20d, %20d]\n", "(signed) short (int)", SHRT_MIN, SHRT_MAX);
+  printf("%25s: [%20d, %20u]\n", "unsigned short (int)", 0, USHRT_MAX);
+  printf("%25s: [%20d, %20d]\n", "(signed) int", INT_MIN, INT_MAX);
+  printf("%25s: [%20d, %20u]\n", "unsigned int", 0, UINT_MAX);
+  printf("%25s: [%20ld, %20ld]\n", "(signed) long (int)", LONG_MIN, LONG_MAX);
+  printf("%25s: [%20ld, %20lu]\n", "unsigned long (int)", 0L, ULONG_MAX);
+  printf("%25s: [%20lld, %20lld]\n", "(signed) long long (int)", LLONG_MIN,
+         LLONG_MAX);
+  printf("%25s: [%20lld, %20llu]\n", "unsigned long long (int)", 0LL,
+         ULLONG_MAX);
+  printf("%25s: [%20E, %20E]\n", "float", FLT_MIN, FLT_MAX);
+  printf("%25s: [%20lE, %20lE]\n", "double", DBL_MIN, DBL_MAX);
+  printf("%25s: [%20LE, %20LE]\n", "long double", LDBL_MIN, LDBL_MAX);
 
-    printf("\n");
-    printf("======================= Ranges from calculations ======================\n");
-    printf("%25s: [%20d, %20d]\n", "(signed) char", ~((unsigned char)~0 >> 1), (unsigned char)~0 >> 1);
-    printf("%25s: [%20d, %20d]\n", "unsigned char", 0, (unsigned char)~0);
-    printf("%25s: [%20d, %20d]\n", "(signed) short (int)", ~((unsigned short)~0 >> 1), (unsigned short)~0 >> 1);
-    printf("%25s: [%20d, %20u]\n", "unsigned short (int)", 0, (unsigned short)~0);
-    printf("%25s: [%20d, %20d]\n", "(signed) int", ~((unsigned int)~0 >> 1), (unsigned int)~0 >> 1);
-    printf("%25s: [%20d, %20u]\n", "unsigned int", 0, (unsigned int)~0);
-    printf("%25s: [%20ld, %20ld]\n", "(signed) long (int)", ~((unsigned long)~0 >> 1), (unsigned long)~0 >> 1);
-    printf("%25s: [%20ld, %20lu]\n", "unsigned long (int)", 0L, (unsigned long)~0);
-    printf("%25s: [%20lld, %20lld]\n", "(signed) long long (int)", ~((unsigned long long)~0 >> 1), (unsigned long long)~0 >> 1);
-    printf("%25s: [%20lld, %20llu]\n", "unsigned long long (int)", 0LL, (unsigned long long)~0);
+  int char_nbits = char_wordlength();
+  char minchar = 1u << (char_nbits - 1), maxchar = ~minchar;
+  unsigned char uminchar = 0u, umaxchar = ~uminchar;
 
-    return 0;
+  int short_nbits = short_wordlength();
+  short minshort = 1u << (short_nbits - 1), maxshort = ~minshort;
+  unsigned short uminshort = 0u, umaxshort = ~uminshort;
+
+  int int_nbits = int_wordlength();
+  int minint = 1u << (int_nbits - 1), maxint = ~minint;
+  unsigned int uminint = 0u, umaxint = ~uminint;
+
+  int long_nbits = long_wordlength();
+  long minlong = 1uL << (long_nbits - 1), maxlong = ~minlong;
+  unsigned long uminlong = 0uL, umaxlong = ~uminlong;
+
+  int longlong_nbits = longlong_wordlength();
+  long long minlonglong = 1uL << (longlong_nbits - 1), maxlonglong = ~minlonglong;
+  unsigned long long uminlonglong = 0uL, umaxlonglong = ~uminlonglong;
+
+  printf("\n");
+  printf("======================= Ranges from calculations\n");
+  printf("%25s (%02d bytes): [%20d, %20d]\n", "(signed) char", char_nbits / 8,
+         minchar, maxchar);
+  printf("%25s (%02d bytes): [%20u, %20u]\n", "unsigned char", char_nbits / 8,
+         uminchar, umaxchar);
+  printf("%25s (%02d bytes): [%20d, %20d]\n", "(signed) short", short_nbits / 8,
+         minshort, maxshort);
+  printf("%25s (%02d bytes): [%20u, %20u]\n", "unsigned short", short_nbits / 8,
+         uminshort, umaxshort);
+  printf("%25s (%02d bytes): [%20d, %20d]\n", "(signed) int", int_nbits / 8,
+         minint, maxint);
+  printf("%25s (%02d bytes): [%20u, %20u]\n", "unsigned int", int_nbits / 8,
+         uminint, umaxint);
+  printf("%25s (%02d bytes): [%20ld, %20ld]\n", "(signed) long", long_nbits / 8,
+         minlong, maxlong);
+  printf("%25s (%02d bytes): [%20lu, %20lu]\n", "unsigned long", long_nbits / 8,
+         uminlong, umaxlong);
+  printf("%25s (%02d bytes): [%20lld, %20lld]\n", "(signed) long long", longlong_nbits / 8,
+         minlonglong, maxlonglong);
+  printf("%25s (%02d bytes): [%20llu, %20llu]\n", "unsigned long long", longlong_nbits / 8,
+         uminlonglong, umaxlonglong);
+
+  return 0;
+}
+
+int char_wordlength(void) {
+  int nbits = 1;
+  unsigned char bits = ~0;
+  while ((bits >>= 1) > 0) {
+    nbits += 1;
+  }
+
+  return nbits;
+}
+
+int short_wordlength(void) {
+  int nbits = 1;
+  unsigned short bits = ~0;
+  while ((bits >>= 1) > 0) {
+    nbits += 1;
+  }
+
+  return nbits;
+}
+
+int int_wordlength(void) {
+  int nbits = 1;
+  unsigned int bits = ~0;
+  while ((bits >>= 1) > 0) {
+    nbits += 1;
+  }
+
+  return nbits;
+}
+
+int long_wordlength(void) {
+  int nbits = 1;
+  unsigned long bits = ~0;
+  while ((bits >>= 1) > 0) {
+    nbits += 1;
+  }
+
+  return nbits;
+}
+
+int longlong_wordlength(void) {
+  int nbits = 1;
+  unsigned long long bits = ~0;
+  while ((bits >>= 1) > 0) {
+    nbits += 1;
+  }
+
+  return nbits;
+}
+
+int float_wordlength(void) {
+  int nbits = 1;
+  float bits = ~0;
+  while ((bits >>= 1) > 0) {
+    nbits += 1;
+  }
+
+  return nbits;
 }
--- solutions/2_07.c
+++ solutions/2_07.c
@@ -1,5 +1,5 @@
 // Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits that begin at
-// position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged. 
+// position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.   
 
 #include <stdio.h>
 

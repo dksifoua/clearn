2d88bb632b66b12262c9ae4af1de9465772cb28b
dksifoua <dimitri.sifoua@gmail.com>
2026-07-30 23:27:21 -0400

    Structs: Point Distance Squared

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-void swap(int* a, int* b) {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+typedef struct point {
+  int x, y;
+} Point;
+
+int distance(Point p1, Point p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
 }
 
 int main(void) {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    swap(&a, &b);
-    printf("%d %d\n", a, b);
-    return 0;
+  int x1, y1, x2, y2;
+  scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+  Point p1 = {.x = x1, .y = y1};
+  Point p2 = {.x = x2, .y = y2};
+
+  printf("%d\n", distance(p1, p2));
+  return 0;
 }

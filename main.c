c27ebb68bd182c94669a85437a80a6925053bee9
dksifoua <dimitri.sifoua@gmail.com>
2026-07-31 22:15:52 -0400

    malloc and free: Allocate and Sum

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-
-typedef struct point {
-  int x, y;
-} Point;
-
-int distance(Point p1, Point p2) {
-    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
-}
+#include <stdlib.h>
 
 int main(void) {
-  int x1, y1, x2, y2;
-  scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-  Point p1 = {.x = x1, .y = y1};
-  Point p2 = {.x = x2, .y = y2};
+    int n;
+    scanf("%d", &n);
 
-  printf("%d\n", distance(p1, p2));
-  return 0;
+    int *arr = malloc(n * sizeof(int));
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", arr + i);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += arr[i];
+    }
+
+    printf("%d\n", sum);
+    free(arr);
+    return 0;
 }
